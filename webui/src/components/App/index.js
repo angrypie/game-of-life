ac13e4db36c57ba81678f98faf7431dd49acaf89
angrypie <cookies.in.chocolate@gmail.com>
2020-07-21 19:47:58 +0300

    separate game component from App

--- webui/src/components/App/index.js
+++ webui/src/components/App/index.js
@@ -1,119 +1,17 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react'
+import React from 'react'
 import './App.css'
-import { observer } from 'mobx-react'
-import { StoreProvider, rootStore, useStore } from '../../models/root'
-
-const Host = 'http://localhost:9090'
+import { StoreProvider, rootStore } from '../../models/root'
+import { Game } from '../game'
 
 const App = () => {
 	return (
 		<StoreProvider value={rootStore}>
-		<div className='App'>
-			<div className='App__Container'>
-				<Board />
-			</div>
-		</div>
-			</StoreProvider>
-	)
-}
-
-const Board = observer(() => {
-	const { game } = useStore()
-	const updateCells = cells => game.updateCells(cells)
-	const updateGrid = grid => game.updateGrid(grid)
-
-	return (
-		<div className='Life'>
-			<div className='Board' style={{ width: `${12 * game.coll}px` }}>
-				{game.cells.map((cell, key) => (
-					<Cell key={key} id={key} state={cell} updateCells={updateCells} />
-				))}
-			</div>
-
-			<div className='Controll'>
-				<Controll updateGrid={updateGrid} updateCells={updateCells} />
-			</div>
-		</div>
-	)
-})
-
-const Cell = ({ id, state, updateCells }) => {
-	return (
-		<div
-			className='Cell'
-			onClick={() => {
-				fetch(`${Host}/state/${id}`, { method: 'POST' })
-					.then(res => res.json())
-					.then(() => {
-						fetch(`${Host}/state`, { method: 'POST' })
-							.then(res => res.json())
-							.then(body => updateCells(body.State))
-					})
-			}}
-		>
-			<div className={state === 1 ? 'Cell__fill' : ''}></div>
-		</div>
-	)
-}
-
-const Controll = ({ updateGrid, updateCells }) => {
-	const inputColl = useRef(null)
-	const inputRow = useRef(null)
-
-	const makeStep = useCallback(() => {
-		fetch(`${Host}/step`, { method: 'POST' })
-			.then(res => res.json())
-			.then(() => {
-				fetch(`${Host}/state`, { method: 'POST' })
-					.then(res => res.json())
-					.then(body => updateCells(body.State))
-			})
-	}, [updateCells])
-
-	const [timer, setTimer] = useState(false)
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (timer) {
-				makeStep()
-			}
-		}, 200)
-		return () => clearInterval(interval)
-	}, [makeStep, timer])
-
-	return (
-		<div className='Controll'>
-			<div className='Controll_size'>
-				<div>
-					<div>Col</div>
-					<input type='text' ref={inputColl} />
+			<div className='App'>
+				<div className='App__Container'>
+					<Game />
 				</div>
-				<div>
-					<div>Row</div>
-					<input type='text' ref={inputRow} />
-				</div>
-				<button
-					onClick={() => {
-						let coll = Number(inputColl.current.value)
-						let row = Number(inputRow.current.value)
-						updateGrid({ coll, row })
-						fetch(`${Host}/game/${coll}/${row}`, { method: 'POST' })
-							.then(res => res.json())
-							.then(() => {
-								fetch(`${Host}/state`, { method: 'POST' })
-									.then(res => res.json())
-									.then(body => updateCells(body.State))
-							})
-					}}
-				>
-					Create
-				</button>
 			</div>
-			<div className='Controll_move'>
-				<button onClick={() => makeStep()}>Next</button>
-				<button onClick={() => setTimer(true)}>Start</button>
-				<button onClick={() => setTimer(false)}>Stop</button>
-			</div>
-		</div>
+		</StoreProvider>
 	)
 }
 
